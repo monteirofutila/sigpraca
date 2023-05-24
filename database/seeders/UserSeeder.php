9947baabe9fc37730a948be6560c6a8a84b0362d
Monteiro Futila <jonquerfutila@gmail.com>
2023-05-24 17:00:13 +0100

    adicionado imagem padrão de usuários

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,6 +6,7 @@ use App\Repositories\UserRepository;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 
 class UserSeeder extends Seeder
 {
@@ -24,7 +25,7 @@ class UserSeeder extends Seeder
             'user_name' => 'admin',
             'email' => 'admin@app.com',
             'password' => Hash::make('password'),
-            'photo' => fake()->imageUrl(),
+            'photo' => $this->imageUploadDefault(),
             'gender' => 'M',
         ]);
         $user->assignRole('Administrador');
@@ -34,7 +35,7 @@ class UserSeeder extends Seeder
             'user_name' => 'caixa',
             'email' => 'caixa@app.com',
             'password' => Hash::make('password'),
-            'photo' => fake()->imageUrl(),
+            'photo' => $this->imageUploadDefault(),
             'gender' => 'M',
         ]);
         $user->assignRole('Caixa');
@@ -44,12 +45,19 @@ class UserSeeder extends Seeder
             'user_name' => 'fiscal',
             'email' => 'fiscal@app.com',
             'password' => Hash::make('password'),
-            'photo' => fake()->imageUrl(),
+            'photo' => $this->imageUploadDefault(),
             'address_street' => fake()->streetAddress(),
             'date_birth' => '2020-03-02',
             'gender' => 'M',
         ]);
         $user->assignRole('Fiscal');
 
+    }
+
+    private function imageUploadDefault()
+    {
+        $file = public_path('assets/images/avatar_user_default.png');
+        $photo_url = Storage::putFile('users', $file);
+        return $photo_url;
     }
 }
