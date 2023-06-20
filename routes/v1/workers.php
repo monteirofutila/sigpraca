437769a38725ca57513cc3c5c98077a7179bccda
Monteiro Futila <jonquerfutila@gmail.com>
2023-06-20 14:37:59 +0100

    implementando edição de trabalhadores

--- routes/v1/workers.php
+++ routes/v1/workers.php
@@ -3,4 +3,5 @@
 use App\Http\Controllers\WorkerController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('workers', WorkerController::class)->middleware(['auth:sanctum']);
+Route::apiResource('workers', WorkerController::class)->middleware(['auth:sanctum'])->except('update');
+Route::post('workers/{workerID}', [WorkerController::class, 'update'])->middleware(['auth:sanctum'])->name('workers.update');
